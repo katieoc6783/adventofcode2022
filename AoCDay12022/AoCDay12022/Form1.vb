87f5c76d34204f0eec75003ed02a931c59cfbf47
katieoc6783 <katie@neiloconnor.com>
2022-12-03 15:46:25 +0000

    slayPt2

--- AoCDay12022/AoCDay12022/Form1.vb
+++ AoCDay12022/AoCDay12022/Form1.vb
@@ -5,7 +5,7 @@ Public Class Form1
 		Dim strReader As StreamReader
 		strReader = File.OpenText("C:\Users\neil\Desktop\AOC 2022\day1Input.txt")
 
-		For i = 0 To linecount - 1
+		For i = 0 To linecount
 			innput(i) = strReader.ReadLine
 		Next
 		strReader.Close()
@@ -13,15 +13,25 @@ Public Class Form1
 
 	Public Sub TotalWithinLoop(ByVal linecount, ByVal innput)
 		Dim tempTotal As Integer = 0
-		Dim max As Integer
+		Dim max1, max2, max3, totalMax, temp, temp2 As Integer
 		'loop
-		For i = 0 To linecount - 1
+		For i = 0 To linecount
 			If innput(i) <> "" Then
 				'add together
 				tempTotal = tempTotal + innput(i)
 			Else
-				If tempTotal > max Then
-					max = tempTotal
+				If tempTotal > max1 Then
+					temp = max1
+					temp2 = max2
+					max2 = temp
+					max3 = temp2
+					max1 = tempTotal
+				ElseIf tempTotal > max2 Then
+					temp = max2
+					max3 = temp
+					max2 = tempTotal
+				ElseIf tempTotal > max3 Then
+					max3 = tempTotal
 				End If
 
 				tempTotal = 0
@@ -29,12 +39,15 @@ Public Class Form1
 			'restart loop
 		Next
 
-		MsgBox(max)
+		'add maxes
+		totalMax = max1 + max2 + max3
+
+		MsgBox(totalMax)
 	End Sub
 
 	Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 		Dim linecount As Integer = File.ReadAllLines("C:\Users\neil\Desktop\AOC 2022\day1Input.txt").Length
-		Dim innput(linecount - 1) As String
+		Dim innput(linecount) As String
 		Dim total As Integer = 0
 
 		Call readInData(linecount, innput)
